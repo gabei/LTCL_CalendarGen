adfe15d25193b0d1aea2ce5dd050e40906954f27
gabei <gabe1792@gmail.com>
2025-07-17 15:46:23 -0500

    Fix up the error messages for the doc builder tests

--- tests/test_doc_builder.py
+++ tests/test_doc_builder.py
@@ -19,7 +19,7 @@ def test_doc_builder_initialization():
 class TestDocBuilderMargins:
     def test_set_margins_negative(self):
         """Test setting negative margins."""
-        with pytest.raises(ValueError):
+        with pytest.raises(ValueError, match=("Margin 'top' cannot be negative.")):
             invalid_margins = {"top": -1,
                                "bottom": 2, "left": 0.25, "right": 3}
             doc_builder = DocBuilder(
@@ -28,7 +28,7 @@ class TestDocBuilderMargins:
     def test_set_margins_invalid_type(self):
         """Test setting margins with an invalid type."""
 
-        with pytest.raises(TypeError):
+        with pytest.raises(TypeError, match="Margins must be a dictionary."):
             invalid_margins = "invalid_type"
             doc_builder = DocBuilder(
                 invalid_margins, valid_font_style, valid_font_size)
@@ -36,7 +36,7 @@ class TestDocBuilderMargins:
     def test_set_margins_missing_keys(self):
         """Test setting margins with missing keys."""
 
-        with pytest.raises(KeyError):
+        with pytest.raises(KeyError, match="Margins dictionary must contain 'right' key."):
             missing_key_margins = {"top": 1, "bottom": 1, "left": 1}
             doc_builder = DocBuilder(
                 missing_key_margins, valid_font_style, valid_font_size)
@@ -46,7 +46,7 @@ class TestDocBuilderFonts:
     def test_set_default_font_style_invalid_type(self):
         """Test setting the default font style with an invalid type."""
 
-        with pytest.raises(TypeError):
+        with pytest.raises(TypeError, match="Font style must be a string."):
             invalid_font_style = 12345
             doc_builder = DocBuilder(
                 valid_margins, invalid_font_style, valid_font_size)
@@ -54,7 +54,7 @@ class TestDocBuilderFonts:
     def test_set_default_font_size_invalid_type(self):
         """Test setting the default font size with an invalid type."""
 
-        with pytest.raises(TypeError):
+        with pytest.raises(TypeError, match="Font style must be a string."):
             invalid_font_size = "large"
             doc_builder = DocBuilder(
                 valid_margins, valid_font_style, invalid_font_size)
